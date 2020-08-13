13fa51f42cb8d7fd14e8c2415fbcdea3fb4035b9
TaehoonJin <54174120+TaeHoon-Jin@users.noreply.github.com>
2020-08-13 20:51:02 +0900

    Add files via upload

---
+++ source/Linear_Reg.R
@@ -0,0 +1,17 @@
+getwd()
+
+setwd('C:/Users/Admin/Desktop/회전교차로 프로젝트/데이터')
+
+df = read.csv('train.csv')
+str(df)
+
+df$sign = as.character(df$sign)
+df$school = as.character(df$school)
+df$roundabout = as.character(df$roundabout)
+
+fit = lm(formula = accident~cctv+sign+speed+peek_speed+car+bus+truck+type+school+roundabout, data=df)
+
+summary(fit)
+
+
+
